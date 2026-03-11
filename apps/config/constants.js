709fc6fab9398e7ba3ef1cc1b2b8a58608cc4b31
nguyenthithuthuy1905 <nguyenthithuthuyy1905@gmail.com>
2026-03-11 09:18:31 +0700

    B2: Add app constants

---
+++ apps/config/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    APP_NAME: "Flower Shop Online",
+    VERSION: "2.0.1",
+    PAGINATION_LIMIT: 12,
+    SUPPORT_EMAIL: "support@flowershop.vn",
+    MAINTENANCE_MODE: false,
+    CURRENCY: "VND"
+};
